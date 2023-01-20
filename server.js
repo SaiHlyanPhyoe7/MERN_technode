7bfe6fb32f643af799f9ee2c404001e729d71e8e
SaiHlyanPhyoe7 <saihlyan.phyoe7@gmail.com>
2023-01-21 04:46:13 +0630

    lecture 4 added userRoute and controller

--- server.js
+++ server.js
@@ -1,13 +1,19 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
 const path = require('path')
-const { logEvents, logger } = require('./middleware/logger')
+const { logger, logEvents } = require('./middleware/logger')
 const errorHandler = require('./middleware/errorHandler')
 const cookieParser = require('cookie-parser')
 const cors = require('cors')
 const corsOptions = require('./config/corsOptions')
-
+const connectDB = require('./config/dbConn')
+const mongoose = require('mongoose')
 const PORT = process.env.PORT || 3500
+
+console.log(process.env.NODE_ENV)
+
+connectDB()
 
 app.use(logger)
 
@@ -20,6 +26,10 @@ app.use(cookieParser())
 app.use('/', express.static(path.join(__dirname, 'public')))
 
 app.use('/', require('./routes/root'))
+app.use('/users', require('./routes/userRoutes'))
+
+
+
 
 app.all('*', (req, res) => {
     res.status(404)
@@ -28,11 +38,18 @@ app.all('*', (req, res) => {
     } else if (req.accepts('json')) {
         res.json({ message: '404 Not Found' })
     } else {
-        res.type('txt').send('404 not Found')
+        res.type('txt').send('404 Not Found')
     }
 })
 
 app.use(errorHandler)
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
+mongoose.connection.once('open', () => {
+    console.log('Connected to MongoDB')
+    app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
+})
 
+mongoose.connection.on('error', err => {
+    console.log(err)
+    logEvents(`${err.no}: ${err.code}\t${err.syscall}\t${err.hostname}`, 'mongoErrLog.log')
+})
